563a114591719b1059547bda28e56ff1f9f8425a
Molly Sarton <MSarton@ucvts.tec.nj.us>
2020-02-12 13:11:03 -0500

    hee hee

---
+++ js/we-gamin.js
@@ -0,0 +1,8 @@
+window.onload = init;
+document.getElementById("tic-tac-toe").onclick = tictactoe;
+document.getElementById("connect-4").onclick = connect4;
+
+
+const tictactoe = function() {
+  
+}
