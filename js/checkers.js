3603eb57890436be9eb23a2a6bfbdb692f291df2
Molly Sarton <msarton@ucvts.org>
2020-03-01 14:35:22 -0500

    even even more checkers

--- js/checkers.js
+++ js/checkers.js
@@ -17,6 +17,24 @@ function init() {
   render();
 }
 
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+  win = getWinner();
+  keepScore(win);
+}
+
 function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;
