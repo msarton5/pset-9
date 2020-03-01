daac7032021308c1684cd593344f8cf7af14c889
Molly Sarton <msarton@ucvts.org>
2020-03-01 14:55:51 -0500

    even even Even even more checkers

--- js/checkers.js
+++ js/checkers.js
@@ -2,15 +2,55 @@ window.onload = function() {
   document.getElementById("board").onclick = takeTurn;
 }
 
+const squares = Array.from(document.querySelectorAll("#board div"));
+
+let board;
+let turn = "black";
+let win;
+
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("x-button").onclick = xFirst;
+document.getElementById("o-button").onclick = oFirst;
+
 function init() {
   board = [
-    "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "",
-    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
   ];
+  board[0] = "\u2022"
+    board[2] = "\u2022"
+    board[4] = "\u2022"
+    board[6] = "\u2022"
+    board[9] = "\u2022"
+    board[11] = "\u2022"
+    board[13] = "\u2022"
+    board[15] = "\u2022"
+    board[16] = "\u2022"
+    board[18] = "\u2022"
+    board[20] = "\u2022"
+    board[22] = "\u2022"
+
+    board[63] = "\u2022"
+    board[61] = "\u2022"
+    board[59] = "\u2022"
+    board[57] = "\u2022"
+    board[54] = "\u2022"
+    board[52] = "\u2022"
+    board[50] = "\u2022"
+    board[48] = "\u2022"
+    board[47] = "\u2022"
+    board[45] = "\u2022"
+    board[43] = "\u2022"
+    board[41] = "\u2022"
+    console.log(board);
   turn = "red";
   win = null;
 
